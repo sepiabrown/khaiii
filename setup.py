37e0b6b895aeda12f023d64b0d374cd537c9894e
Suwon Park <sepiabrown@naver.com>
2020-12-16 08:15:04 +0000

    change dir

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ def readme():
     Returns:
         long description (content of README.md)
     """
-    return open('README.md', 'r', encoding='UTF-8').read()
+    return open('./README.md', 'r', encoding='UTF-8').read()
 
 
 #########
